3e3fb64055df8f86e3abb3157be2e07a7cf0b45c
Lindsey Slagle <slaglel@oregonstate.edu>
2024-10-11 22:50:34 -0700

    temp convert

---
+++ temp_convert.py
@@ -0,0 +1,9 @@
+# Author: Lindsey Michelle Slagle
+# GitHub username: Lswaggerty
+# Date: 10/11/2024
+# Description: This program converts given Celsius temperatures to Fahrenheit temperatures
+print("Please enter a Celsius temperature")
+celsius = float(input())
+fahrenheit = (9/5) * celsius + 32
+print("The equivalent Fahrenheit temperature is:")
+print(fahrenheit)

cad7f775aa95be03cca073b44c6d8d823e7dec2f
Lindsey Slagle <slaglel@oregonstate.edu>
2024-10-11 22:30:31 -0700

    average py

---
+++ average.py
@@ -0,0 +1,13 @@
+# Author: Lindsey Michelle Slagle
+# GitHub username: Lswaggerty
+# Date: 10/11/2024
+# Description: This program asks the user for five numbers, calculates the average, and prints the average
+print("please enter five numbers.")
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+average = (num1 + num2 + num3 + num4 + num5) / 5
+print("The average of those numbers is:")
+print(f"{average:.1f}")

0392284ea6fc8c02e49e5fbe2c3b05b3e8c352c4
Lindsey Slagle <slaglel@oregonstate.edu>
2024-10-11 22:53:41 -0700

    temp convert

--- temp_convert.py
+++ temp_convert.py
@@ -2,7 +2,7 @@
 # GitHub username: Lswaggerty
 # Date: 10/11/2024
 # Description: This program converts given Celsius temperatures to Fahrenheit temperatures
-print("Please enter a Celsius temperature")
+print("Please enter a Celsius temperature.")
 celsius = float(input())
 fahrenheit = (9/5) * celsius + 32
 print("The equivalent Fahrenheit temperature is:")

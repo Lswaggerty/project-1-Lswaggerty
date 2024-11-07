02706d97453a12329e2aea4cd81d7b5d9a5112c4
Lindsey Slagle <slaglel@oregonstate.edu>
2024-11-06 17:39:22 -0800

    Find the median

--- find_median.py
+++ find_median.py
@@ -3,8 +3,9 @@
 # Date: 11/6/2024
 # Description: Create a function that takes a list of numbers and returns with the median of the list of numbers
 def find_median():
-    """Return the statistical median of a predefined list of numbers."""
-    numbers = [13, 7, -3, 82, 4]  # predefined list of values
+    """Return the statistical median of a user-provided list of numbers."""
+    numbers = input("Enter a list of numbers separated by spaces: ")
+    numbers = list(map(float, numbers.split()))
     if not numbers:
         return None
     sorted_numbers = sorted(numbers)
@@ -16,7 +17,8 @@ def find_median():
         return (mid1 + mid2) / 2
 
 # Example usage
-result = find_median()  # result should be 7
+result = find_median()
 print("Median:", result)
+
 
 
